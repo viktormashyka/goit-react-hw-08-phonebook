2788bc79282e2bf77f2e9af39e30a648eb7acfac
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-11-25 05:45:38 +0000

    update router

--- src/index.js
+++ src/index.js
@@ -21,12 +21,15 @@ import { store, persistor } from './redux/store';
 import './index.css';
 
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
